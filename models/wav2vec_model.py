7b0a57765033ddba2056cab5b942ccf60a735d1b
Ajit Nandi <mrajit2772@gmail.com>
2026-03-04 10:29:31 +0530

    fix wav2vec model loading

--- models/wav2vec_model.py
+++ models/wav2vec_model.py
@@ -1,7 +1,7 @@
 import torch
 from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2Model
 
-MODEL_PATH = "pretrained/wav2vec2-base"
+MODEL_PATH = "facebook/wav2vec2-base"
 
 feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(MODEL_PATH)
 wav2vec = Wav2Vec2Model.from_pretrained(MODEL_PATH)
@@ -9,6 +9,7 @@ wav2vec = Wav2Vec2Model.from_pretrained(MODEL_PATH)
 wav2vec.eval()
 
 def extract_features(audio, sr=16000):
+
     inputs = feature_extractor(
         audio,
         sampling_rate=sr,
@@ -19,6 +20,7 @@ def extract_features(audio, sr=16000):
     with torch.no_grad():
         outputs = wav2vec(**inputs)
 
-    # ✅ mean pooling (IMPORTANT)
+    # mean pooling
     features = outputs.last_hidden_state.mean(dim=1)
+
     return features
